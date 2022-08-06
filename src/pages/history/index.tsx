29e8deaa7049ab23d9300a9379fd27f8fca867f3
Guilherme Junio <guilhermejunios@gmail.com>
2022-08-06 20:07:58 -0300

    Componente status

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './style'
+import { HistoryContainer, HistoryList, Status } from './style'
 
 export function History() {
   return (
@@ -18,19 +18,25 @@ export function History() {
               <td>Tarefa</td>
               <td>20min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20min</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
