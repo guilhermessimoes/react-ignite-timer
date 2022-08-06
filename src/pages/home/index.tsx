eb778e75484800a39dfb5b776029779dbbd94794
Guilherme Junio <guilhermejunios@gmail.com>
2022-08-06 19:37:50 -0300

    Aprimorando os inputs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 01" />
+            <option value="Projeto 02" />
+            <option value="Projeto 03" />
+            <option value="Peixe" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
